4eb6cab027358dbfdebe1a590d6e8d9d7040ac0e
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-03-25 05:25:22 +0000

    task 101

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,10 @@
 export default function iterateThroughObject(reportWithIterator) {
-
+  let returnValue = '';
+  for (let i = 0; i < reportWithIterator.length; i += 1) {
+    returnValue += reportWithIterator[i];
+    if (i !== reportWithIterator.length - 1) {
+      returnValue += ' | ';
+    }
+  }
+  return returnValue;
 }
