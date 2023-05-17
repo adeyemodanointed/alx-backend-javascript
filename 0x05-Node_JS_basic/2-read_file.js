e50b39992da9d70a5305eab12613bcaf50d92983
Peter Adeyemo <adeyemo@tm30.net>
2023-05-17 05:28:39 +0100

    task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,3 +1,43 @@
-function countStudents(path) {
-  const fs = require('fs')  
+function countStudents (path) {
+  const fs = require('fs');
+
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const lines = data.toString().split('\n');
+    const fields = {};
+    const students = {};
+    let dataLength = 0;
+
+    lines.forEach((line) => {
+      if (line) {
+        dataLength += 1;
+        const field = line.split(',');
+        if (Object.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
+        }
+        if (Object.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
+      }
+    });
+
+    console.log(`Number of students: ${dataLength - 1}`);
+    for (const [key, value] of Object.entries(fields)) {
+      if (key !== 'field') {
+        console.log(
+          `Number of students in ${key}: ${value}. List: ${students[key].join(
+            ', '
+          )}`
+        );
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
+
+module.exports = countStudents;
